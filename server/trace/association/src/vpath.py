69bf8069aaea9991d2b6cb58b92990a35c748ad5
thu-gyt <2740923038@qq.com>
2025-05-23 14:41:06 +0800

    add vpath algo

--- server/trace/association/src/vpath.py
+++ server/trace/association/src/vpath.py
@@ -1,3 +1,4 @@
+from utils import intra_preprocess
 class Event(object):
     def __init__(
         self,
@@ -16,7 +17,8 @@ class Event(object):
         self.event_kind = event_kind
         self.sort_key = sort_key
         self.span = span
-        self.association_tracid = 0
+        self.association_tracid = None
+        self.parent_id = None
 
     def __repr__(self):
         return "Event:(%d, %s, %d, %s, %s)" % (
@@ -32,43 +34,47 @@ class Event(object):
 
 
 def vpath(spans):
-    processed_spans = {}
-    for tgid, span_list in spans.items():
+
+    span_dict = intra_preprocess(spans)
+    for tgid, span_list in span_dict.items():
         if len(span_list['incoming']) == 0 or len(span_list['outgoing']) == 0:
             continue
 
         output_span_count = 0
-        processed_spans[tgid] = []
 
+        span2event = {}
         # collect events
         all_events = {}
-        for incoming_ip, incoming_spans in span_list['incoming'].items():
-            for span in incoming_spans:
+        for in_ip, in_spans in span_list['incoming'].items():
+            for span in in_spans:
                 if span.pid not in all_events:
                     all_events[span.pid] = []
                 start_event = Event(span.pid, span.trace_id, span.start_time, "incoming", "request", 1, span)
                 all_events[span.pid].extend([start_event])
 
-                span.start_event = start_event
-        for outgoing_ip, outgoing_spans in span_list['outgoing'].items():
-            for span in outgoing_spans:
+                span2event[span] = start_event
+        for out_ip, out_spans in span_list['outgoing'].items():
+            for span in out_spans:
                 if span.pid not in all_events:
                     all_events[span.pid] = []
                 start_event = Event(span.pid, span.trace_id, span.start_time, "outgoing", "request", 2, span)
                 all_events[span.pid].extend([start_event])
 
-                span.start_event = start_event
+
+                span2event[span] = start_event
             
         # match events between incoming_spans' (start_event, end_event) 
         for pid, events in all_events.items():
             all_events[pid] = sorted(events, key=lambda x: (float(x.event_time), x.sort_key))
 
-        for incoming_ip, incoming_spans in span_list['incoming'].items():
-            for span in incoming_spans:
+     
+        for in_ip, in_spans in span_list['incoming'].items():
+            for span in in_spans:
                 pid = span.pid
                 start_time = span.start_time
                 end_time = span.end_time
                 trace_id = span.trace_id
+                span_id = span.span_id
 
                 for event in all_events[pid]:
                     if event.event_time > end_time:
@@ -77,13 +83,18 @@ def vpath(spans):
                         continue
                     
                     event.association_tracid = trace_id
+                    # event.parent_id = span_id
+                    for span, event2 in span2event.items():
+                        if event == event2:
+                            event2.parent_id = span.span_id
+                            break
                         
+            for outgoing_ip, outgoing_spans in span_list['outgoing'].items():
+                for span in outgoing_spans:
+                    start_event = span2event[span]
+                    # span.association_tracid = start_event.association_tracid
+                    span.parent_id = start_event.parent_id
+                    # print(f"span_id: {span.span_id}, parent_id: {span.parent_id} start_event: {start_event.parent_id}")
+    
 
-        for outgoing_ip, outgoing_spans in span_list['outgoing'].items():
-            for span in outgoing_spans:
-                start_event = span.start_event
-
-                span.association_tracid = start_event.association_tracid
-                processed_spans[tgid].append(span)
-
-    return processed_spans
+    return span_dict
