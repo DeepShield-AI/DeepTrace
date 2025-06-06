4b391baf58df99817f269b4e080d16ad6f9a8e8f
thu-gyt <2740923038@qq.com>
2025-06-06 14:42:58 +0800

    add proxy for rust install

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,6 +9,8 @@ sudo apt-get install -y curl build-essential gcc
 
 # Install Rust non-interactively
 echo "Starting Rust installation..."
+export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
+export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain=stable
 
 # Set up environment variables
