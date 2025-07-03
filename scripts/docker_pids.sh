21394b5ab159b4d03c1358e512e3e1b00c2760ff
thu-gyt <2740923038@qq.com>
2025-07-03 13:18:31 +0800

    filter wrk2 container

--- scripts/docker_pids.sh
+++ scripts/docker_pids.sh
@@ -15,9 +15,14 @@ if [ -z "$container_ids" ]; then
     exit 0
 fi
 
-
 # 遍历每个容器，获取其 PID
 for container_id in $container_ids; do
+    # 获取容器名称
+    name=$(docker inspect --format '{{.Name}}' "$container_id" | sed 's|/||')
+    # 如果名称包含 wrk，则跳过
+    if [[ "$name" == *wrk* ]]; then
+        continue
+    fi
     # 获取容器的 PID
     pid=$(docker inspect --format '{{.State.Pid}}' "$container_id")
     echo "$pid"
