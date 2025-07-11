fc43e648acaa5f8017041ef2bf59f14fdd9a6f14
thu-gyt <2740923038@qq.com>
2025-07-11 13:04:22 +0800

    add clear script

---
+++ scripts/clear.sh
@@ -0,0 +1,19 @@
+
+
+
+
+docker exec -it deeptrace_server python -m cli.src.cmd agent stop
+
+docker exec -it deeptrace_server python -m cli.src.cmd agent uninstall_app
+
+
+cd ./server
+# 停止并移除容器
+sudo docker-compose down
+
+# 清空数据、日志、插件目录
+sudo rm -rf /user/share/es/data
+sudo rm -rf /user/share/es/logs
+sudo rm -rf /user/share/es/plugins
+
+echo "Elasticsearch 相关目录已清空并重建。"
