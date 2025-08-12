8b0f9c33728bde75083b86c62f8c8000f8771a7d
gyt <86474526+thu-gyt@users.noreply.github.com>
2025-08-12 13:38:46 +0800

    Update deploy.sh

--- tests/workload/socialnetwork/deploy.sh
+++ tests/workload/socialnetwork/deploy.sh
@@ -5,8 +5,23 @@ DOCKER_CONFIG="/etc/docker/daemon.json"
 TARGET_REGISTRY="47.97.67.233:5000"
 TEMP_FILE=$(mktemp)
 
-sudo apt-get update
-sudo apt-get install -y jq docker.io docker-compose
+
+if [ ! -x /usr/bin/jq ]; then
+    echo "installing jq ..."
+    sudo apt-get update
+    sudo apt-get install -y jq
+else
+    echo "jq 已安装"
+fi
+
+if [ ! -x /usr/bin/docker ]; then
+    echo "installing docker.io..."
+    sudo apt-get update
+    sudo apt-get install -y docker.io docker-compose
+else
+    echo "docker.io 已安装"
+fi
+
 
 if [ ! -f "$DOCKER_CONFIG" ] || [ ! -s "$DOCKER_CONFIG" ]; then
     echo "{}" | sudo tee "$DOCKER_CONFIG" >/dev/null
