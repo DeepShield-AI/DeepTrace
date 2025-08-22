b25df49c15eb4d090c1f78d06cfc6e2d1479e986
thu-gyt <2740923038@qq.com>
2025-08-22 15:20:58 +0800

    add demo2 test

--- server/trace/model/span.py
+++ server/trace/model/span.py
@@ -67,6 +67,9 @@ class Span:
             match = re.search(r'Request-Id:\s*(\d+)', content)
             if match:
                 return match.group(1)
+            match = re.search(r'Requestid:\s*(\d+)', content)
+            if match:
+                return match.group(1)
         if 'x-b3-traceid' in content:
             match = re.search(r"x-b3-traceid:\s*([0-9a-fA-F]+)", content)
             if match:
